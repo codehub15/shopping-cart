bce17fbaea4b9228b243d7e4c98f88389ba7b363
natalialexandra <natalia.alexandra195@gmail.com>
2020-11-12 13:50:57 +0100

    add cart

---
+++ src/components/cart/CartItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartItem() {
+    return (
+        <div>
+            <h2>cart item</h2>
+        </div>
+    )
+}
---
+++ src/components/cart/CartProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CartProduct() {
+    return (
+        <div>
+            <h2>cart product</h2>
+        </div>
+    )
+}
