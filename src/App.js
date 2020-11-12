63ef616a83e987d7bbacbf9516e77782ddee6d4d
natalialexandra <natalia.alexandra195@gmail.com>
2020-11-12 10:12:04 +0100

    set up

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './style/app'
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>App</h1>
+        </div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// import store from './redux/store'
+// import { Provider } from 'react-redux'
+
+// <Provider store={store}>   </Provider>
+
+ReactDOM.render(
+    <React.StrictMode>
+
+        <App />
+
+    </React.StrictMode>,
+    document.getElementById('root')
+);
