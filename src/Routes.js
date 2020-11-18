8f73e220402cd46ea302fa3fe99c55aaa9f29a34
natalialexandra <natalia.alexandra195@gmail.com>
2020-11-18 19:56:21 +0100

    add routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
+import Cart from './components/cart/Cart'
+import SingleProduct from './components/products/SingleProduct'
+import Products from './components/products/Products'
+import NotFound from './components/NotFound'
+import Auth from './components/user/Auth'
 
 export default function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-
+            <Route path="/products" component={Products} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/product/:id" component={SingleProduct} />
+            <Route path="/auth" component={Auth} />
+            <Route component={NotFound} />
         </Switch>
     )
 }
