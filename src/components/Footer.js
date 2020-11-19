e34ef38946b6e0b60ecb5e854f43532896750c57
natalialexandra <natalia.alexandra195@gmail.com>
2020-11-19 09:50:18 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../style/footer.css'
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>Build with  ♡ by Natalia Alexandra</p>
+        </footer>
+    )
+}
