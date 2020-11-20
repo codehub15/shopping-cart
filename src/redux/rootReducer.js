a8fa7ed8e1c8ee80bcee228b0a13df5e75d6610c
natalialexandra <natalia.alexandra195@gmail.com>
2020-11-20 17:01:04 +0100

    add user

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
-import shoppingReducer from './shopping-reducer'
+import shoppingReducer from './shop/shopping-reducer'
+import userReducer from './user/user-reducer'
 
 const rootReducer = combineReducers({
-    shop: shoppingReducer
+    shop: shoppingReducer,
+    user: userReducer
 })
 
 export default rootReducer;
